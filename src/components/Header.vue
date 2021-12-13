8124ebdfcbd9473fae81f14a35c5e61276636202
LarisaGimbu <gimbularisa@gmail.com>
2021-12-13 12:29:57 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,19 @@
 <template>
-  <header>
-    <div class="header-top">
-
+  <header class="container">
+    <div class="header-top d-flex justify-content-between align-intems-center">
+      <ul>
+        <li class="pe-3"><a href="#">Download App</a></li>
+        <li><a href="#">Advertise with us</a></li>
+      </ul>
+      <ul>
+        <li>SOCIAL</li>
+      </ul>
     </div>
+    <div class="logo d-flex justify-content-center">
+      <img src="../assets/img/avada-food-logo.png" alt="">
+    </div>
+
+    <nav>NAVBAR</nav>
   </header>
 </template>
 
@@ -13,4 +24,17 @@ export default {
 </script>
 
 <style lang="scss">
+.header-top{
+  ul{
+    list-style: none;
+    li{
+      display: inline-block;
+      a{
+        text-decoration: none;
+        font-size: 0.8rem;
+      }
+    }
+      
+  }
+}
 </style>
