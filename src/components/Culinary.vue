b4b8c119d11fed71ac2996967f1bda1fdbcb44d8
LarisaGimbu <gimbularisa@gmail.com>
2021-12-14 14:59:56 +0100

    culinary-collection

--- src/components/Culinary.vue
+++ src/components/Culinary.vue
@@ -4,7 +4,15 @@
       <h3>CULINARY COLLECTION</h3>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br/> Quam mollitia nam dignissimos facilis voluptatibus.</p>
     </div>
-    <h3>CulinaryCARD in v-for</h3>
+    <div class="culinary-cards d-flex flex-wrap justify-content-between pt-5">
+      <div 
+      v-for="card in 8"
+      :key="card"
+      class="culinary-card text-center">
+        <img src="../assets/img/drinks-recipes.png" alt="">
+        <p>DRINKS</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +22,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .culinary-cards{
+    width: 100%;
+    .culinary-card{
+      width: 22%;
+      background-color: white;
+      padding: 10px 0 10px 0;
+      margin-bottom: 25px;
+      img{
+        width: 40%;
+      }
+    }
+  }
 
 </style>
