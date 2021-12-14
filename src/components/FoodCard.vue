8a82b08bf4de00ae18d67a304634b604b25259cb
LarisaGimbu <gimbularisa@gmail.com>
2021-12-14 12:43:49 +0100

    foodCard

---
+++ src/components/FoodCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="cards d-flex justify-content-around">
+    <div class="my-card">
+      <img src="../assets/img/single-post-img3-1200x790.jpg" alt="">
+      <div class="description text-cxenter">
+        <h4 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing</h4>
+        <p class="text-center">Data</p>
+      </div>
+    </div>
+    <div class="my-card">
+      <img src="../assets/img/single-post-img3-1200x790.jpg" alt="">
+      <div class="description text-cxenter">
+        <h4 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing</h4>
+        <p class="text-center">Data</p>
+      </div>
+    </div>
+    <div class="my-card">
+      <img src="../assets/img/single-post-img3-1200x790.jpg" alt="">
+      <div class="description text-cxenter">
+        <h4 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing</h4>
+        <p class="text-center">Data</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FoodCard'
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/general.scss';
+@import '../assets/style/vars.scss';
+
+.my-card{
+  width: 30%;
+  img{
+    width: 100%;
+  }
+  .description{
+    border: none;
+    width: 75%;
+    margin: 0 auto;
+    p{
+      color: $grey-text;
+    }
+  }
+}
+
+</style>
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -6,16 +6,33 @@
       <h1>Lorem ipsum dolor sit amet consectetur.</h1>
       <p class="date">March 25, 2019</p>
     </div>
-    <div class="container text-center foodies">
-      <h5>FOODIE JOURNAL</h5>
-      <div>CARD</div>
+    <div class="container foodies">
+      <div class="foodie-title d-flex justify-content-center align-items-center">
+        <span class="points" v-for="punto in 120" :key="punto">.</span>
+        <h5 class="ps-5 pe-5">FOODIE JOURNAL</h5>
+        <span class="points" v-for="punto in 120" :key="punto">.</span>
+      </div>
+      <FoodCard />
     </div>
   </div>
 </template>
 
 <script>
+import FoodCard from './FoodCard.vue'
+
 export default {
-  name: 'Jumbotron'
+  name: 'Jumbotron',
+  components:{
+    FoodCard
+  },
+  data(){
+    return{
+      foodImg:['single-post-img3-1200x790.jpg', ],
+      foodTitle:['Lorem'],
+      foodDate:['2019']
+    }
+  }
+  
 }
 </script>
 
@@ -26,7 +43,7 @@ export default {
 .jumbotron{
   width: 100%;
   min-height: 80vh;
-  background-image: url("../assets/img/single-post-img3-1200x790.jpg");
+  background-image: url("../assets/img/single-post-img3.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
@@ -48,11 +65,15 @@ export default {
     }
     .date{
       color: $grey-text;
+    } 
+  }
+  .foodies{
+    background-color: white;
+    .points{
+      font-size: 0.8rem;
+      padding-bottom: 15px;
+      color: #E0DEDE;
     }
-    .foodies{
-      background-color: white;
-    }
-    
   }
 }
 
